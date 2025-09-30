9d7f220a48df3463a4e15cf2ee927595a29180f6
Andrew Pikul <ajpikul@gmail.com>
2025-09-30 14:24:05 -0500

    Get full roundtrip for touch working.

--- src/tetsuya/app/services/utils/config.py
+++ src/tetsuya/app/services/utils/config.py
@@ -3,8 +3,8 @@
 from __future__ import annotations
 
 import tomllib
+from http import HTTPStatus
 from pathlib import Path
-from typing import TYPE_CHECKING
 
 import logistro
 import platformdirs
@@ -12,9 +12,6 @@ import typer
 
 from tetsuya._globals import app, cli
 from tetsuya.app.client import get_client
-
-if TYPE_CHECKING:
-    from fastapi import Request
 
 _logger = logistro.getLogger(__name__)
 
@@ -38,19 +35,27 @@ cli.add_typer(config_cli, name="config")
 def touch(*, default: bool = False, overwrite: bool = False):
     """Create config file if it doesn't exist."""
     client = get_client()
-    response = client.put(
+    _logger.debug("Sending touch command.")
+    r = client.put(
         "/config/touch",
         json={"default": default, "overwrite": overwrite},
     )
-    result = response.json()
-    print(result.get("path", "Some error occurred"))  # noqa: T201
+    _logger.debug("Processing touch response")
+    # check return value
+    if r.status_code == HTTPStatus.OK:
+        result = r.json()
+        print(result.get("path", f"Weird result: {result}"))  # noqa: T201
+    else:
+        raise ValueError(f"{r.status_code}: {r.text}")
 
 
 @app.put("/config/touch")
-async def _touch(request: Request):
+async def _touch(data: dict):
     """Create the config file if it doesn't exist."""
     _logger.info("Touching config file.")
-    _data = await request.json()  # (should we get defaults)
+    _logger.info(f"Touch received data: {data}")
     config_file.parent.mkdir(parents=True, exist_ok=True)
     config_file.touch()
-    return {"path": str(config_file.resolve())}
+    ret = {"path": str(config_file.resolve())}
+    _logger.info(f"Touch sending back: {ret}")
+    return ret
