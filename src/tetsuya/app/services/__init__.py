3c507a41fa36b3ae97e2058a38722a6fc3b636dd
Andrew Pikul <ajpikul@gmail.com>
2025-09-30 17:10:05 -0500

    Add service list function.

--- src/tetsuya/app/services/__init__.py
+++ src/tetsuya/app/services/__init__.py
@@ -1,16 +1,41 @@
-"""Services containst the services plus their utilities."""
+"""services contains the services plus their utilities."""
 
-from tetsuya._globals import cli
+from http import HTTPStatus
+
+import logistro
+import typer
+
+from tetsuya._globals import active_services, app, cli
+from tetsuya.app.client import get_client
 
 from . import utils as utils
 from .search_git import SearchGit
 
 __all__ = ["SearchGit"]
 
+_logger = logistro.getLogger(__name__)
 
-@cli.command(name="service")
-def _service(name: str):
-    # from here, we make a client call to the server
-    # are capacity to do that is in _server
-    # its a matching endpoint
-    pass
+service_cli = typer.Typer(help="Commands for managing the services.")
+cli.add_typer(service_cli, name="service")
+
+
+@service_cli.command(name="list")  # accept --all
+def _list():
+    """Reload config file."""
+    client = get_client()
+    _logger.debug("Sending reload command.")
+    r = client.post(
+        "/service/list",
+    )
+    # check return value
+    if r.status_code == HTTPStatus.OK:
+        for s in r.json():
+            print(s)  # noqa: T201
+    else:
+        raise ValueError(f"{r.status_code}: {r.text}")
+
+
+@app.post("/service/list")
+async def _reload():
+    """Reload config file."""
+    return list(active_services.keys())
