7b67753fe0dd656ab6194773db4341cd05f2964f
Andrew Pikul <ajpikul@gmail.com>
2025-11-02 18:30:23 -0500

    Reorganize server and client files.

--- src/tetsuya/app/__init__.py
+++ src/tetsuya/app/__init__.py
@@ -1,16 +1,8 @@
-"""app providers the server and its services, as well as script entry."""
+"""
+Provides the tetsuya server and client.
 
-import logistro
+Submodule server.py is most of both interfaces, containing most of the client. Even
+client.py is just helper functions.
+"""
 
-from tetsuya._globals import cli
-
-from . import server as server  # need this to be executed
 from . import services as services  # need this to be executed
-
-_logger = logistro.getLogger(__name__)
-
-
-def main():  # script entry point
-    """Start the cli service."""
-    _, remaining = logistro.parser.parse_known_args()
-    cli(args=remaining)
--- src/tetsuya/app/utils.py
+++ src/tetsuya/app/utils.py
@@ -1,7 +1,10 @@
 """Utitilies for everyone. No deps."""
 
+import atexit
+from http import HTTPStatus
 from pathlib import Path
 
+import httpx
 import logistro
 import platformdirs
 
@@ -18,3 +21,47 @@ def uds_path() -> Path:
     p.parent.mkdir(parents=True, exist_ok=True)
     _logger.info(f"Socket path: {p!s}")
     return p
+
+
+def is_server_alive(uds_path: Path) -> bool:
+    """Check if server is running."""
+    if not uds_path.exists():
+        return False
+    client: None | httpx.Client = None
+    try:
+        client = get_client(uds_path, defer_close=False)
+        r = client.get("/ping")
+        if r.status_code == HTTPStatus.OK:
+            _logger.info("Socket ping returned OK- server alive.")
+            return True
+        else:
+            _logger.info(
+                f"Socket ping returned {r.status_code}, removing socket.",
+            )
+            uds_path.unlink()
+            return False
+    except httpx.TransportError:
+        _logger.info("Transport error in socket, removing socket.")
+        uds_path.unlink()
+        return False
+    finally:
+        if client:
+            client.close()
+
+
+def get_client(
+    path: Path | None = None,
+    *,
+    defer_close=True,
+    timeout=0.05,
+) -> httpx.Client:
+    """Get a client you can use for executing commands."""
+    transport = httpx.HTTPTransport(uds=str(path or uds_path()))
+    client = httpx.Client(
+        timeout=httpx.Timeout(timeout),
+        transport=transport,
+        base_url="http://tetsuya",
+    )
+    if defer_close:
+        atexit.register(client.close)
+    return client
